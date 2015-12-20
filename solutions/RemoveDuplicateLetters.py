90075806bea2c15d1ede1245675c86c199aeb26a
Daoyuan Jia <this@caunion.me>
2015-12-20 19:42:26 +0800

    Finished Stack sections

--- solutions/RemoveDuplicateLetters.py
+++ solutions/RemoveDuplicateLetters.py
@@ -2,6 +2,7 @@ from BaseSolution import *
 class RemoveDuplicateLetters(BaseSolution):
     def __init__(self):
         BaseSolution.__init__(self)
+        self.fuckinglevel = 9
         self.push_test(
             params = ("cbacdcbc",),
             expects = "acdb"
@@ -24,3 +25,22 @@ class RemoveDuplicateLetters(BaseSolution):
         prev += c
         s = s[p+1:].replace(c, '')
         return self.removeDupe(s, prev)
+
+    def solution(self, s):
+        visited = [False] * 26
+        stack = []
+        count = [0] * 26
+        for c in s:
+            count[ord(c) - ord('a')] += 1
+        for c in s:
+            index = self.char2num(c)
+            count[index] -= 1
+            if visited[index] : continue
+            while len(stack) > 0 and stack[-1] > c and count[self.char2num(stack[-1])] > 0:
+                top = stack.pop()
+                visited[self.char2num(top)] = False
+            stack += c
+            visited[index] = True
+        return "".join(stack)
+    def char2num(self, c):
+        return ord(c) - ord('a')
--- solutions/TrappingRainWater.py
+++ solutions/TrappingRainWater.py
@@ -10,5 +10,22 @@ class TrappingRainWater(BaseSolution):
         )
 
     def solution(self, height):
-        idx = 0
-        length = len(height)
+        if not height or len(height) < 2: return 0
+        stack = []
+        ret = 0
+        n = len(height)
+        for i in range(0, n):
+            if len(stack) == 0 or height[stack[-1]] > height[i]:
+                stack.append(i)
+            elif height[stack[-1]] == height[i]:
+                stack.pop()
+                stack.append(i)
+            else:
+                while len(stack) > 0 and height[stack[-1]] < height[i]:
+                    top = stack.pop()
+                    if len(stack) > 0:
+                        h = (min(height[stack[-1]], height[i]) - height[top] )
+                        w = (i - stack[-1] - 1)
+                        ret += (h*w)
+                stack.append(i)
+        return ret
